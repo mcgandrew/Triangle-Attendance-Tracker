ca63d2e2baf9b1a448eaf8cb3ccaaafb6750f8cf
mcgandrew <andrew.mcgann7@yahoo.com>
2024-10-07 19:29:20 -0700

    Implemented User model and controller

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions options) : base(options) { }
 
     public DbSet<Event> Events => Set<Event>();
+    public DbSet<User> Users => Set<User>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
